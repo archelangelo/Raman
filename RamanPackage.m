2d6cc5df2b209b547e70f68e7ea487330af3e8c6
Archelangelo <medycc@gmail.com>
2015-10-27 20:03:50 -0400

    In this version, a small bug is fixed, not essential.

--- RamanPackage.m
+++ RamanPackage.m
@@ -13,7 +13,7 @@ function [varargout] = RamanPackage(B, ele, varargin)
     for k = 1:nVargs
         C = BackSub(varargin{k}, B, 1700, 2100, 0, false);
         varargout{k} = [varargin{k}(:, 1), C];
-        plot(varargin{k}(:, 1), C + ele * (k + 1), varargin{k + nVargs});
+        plot(varargin{k}(:, 1), C + ele * (k - 1), varargin{k + nVargs});
         [p, h, w] = PeakAnalyse(varargout{k}, 2570, 2875);
         fprintf('Spectrum %d\n', k);
         fprintf('\tPeak at %.3f\n\tHeight: %.3f\n\tFWHM: %.3f\n', p, h, w);
