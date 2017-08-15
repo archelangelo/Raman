dbbced474e502b14917c6bfbc6a2234a4d10eb04
Yiran Hu <medycc@gmail.com>
2017-08-15 15:25:46 -0400

    Fixed G-S2 Ratio calculation and performed min subtraction to all spectra.

--- PackageWrapper.m
+++ PackageWrapper.m
@@ -13,12 +13,12 @@ function [A, C] = PackageWrapper(filename, background, ele, showpk, findpk)
     B = importdata(background);
     if isnan(X(1, 1))
         if isnan(X(1, 2))
-            A = zeros(size(X, 2) - 2, 2);
+            A = zeros(size(X, 2) - 2, 2, size(X, 1)-1);
             for i = 2:size(X, 1)
                 A(:, :, i - 1) = [X(1, 3:end); X(i, 3:end)]';
             end
         else
-            A = zeros(size(X, 2) - 1, 2);
+            A = zeros(size(X, 2) - 1, 2, size(X, 1)-1);
             for i = 2:size(X, 1)
                 A(:, :, i - 1) = [X(1, 2:end); X(i, 2:end)]';
             end
@@ -26,14 +26,18 @@ function [A, C] = PackageWrapper(filename, background, ele, showpk, findpk)
     else
         A = X;
     end
+    B(:, 2) = B(:, 2) - min(B(:, 2));
+    A(:, 2, :) = A(:, 2, :) - ...
+        repmat(min(A(:, 2, :), [], 1), size(A, 1), 1, 1);
     C = RamanPackage(B, ele, A, showpk, findpk);
     mask_S1 = B(:, 1) > 1460 & B(:, 1) < 1660;
     mask_S2 = B(:, 1) > 1660 & B(:, 1) < 1960;
     s12ratio_back = trapz(B(mask_S1, 1), B(mask_S1, 2), 1) / ...
         trapz(B(mask_S2, 1), B(mask_S2, 2), 1);
-    s12ratio = trapz(A(mask_S1, 1), A(mask_S1, 2:end), 1) ./ ...
-        trapz(A(mask_S2, 1), A(mask_S2, 2:end), 1);
-    gs2ratio = s12ratio - s12ratio_back;
-    disp('G/S_1 Ratio:');
+    s12ratio = trapz(A(mask_S1, 1, 1), A(mask_S1, 2, :), 1) ./ ...
+        trapz(A(mask_S2, 1, 1), A(mask_S2, 2, :), 1);
+    s12ratio = reshape(s12ratio, 1, size(A, 3));
+    gs2ratio = s12ratio - repmat(s12ratio_back, 1, size(s12ratio, 2));
+    disp('G/S_1 ratio:');
     disp(gs2ratio);
 end
